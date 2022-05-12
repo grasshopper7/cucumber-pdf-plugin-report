5ea16ab1019fe24cf0b658ba3efd3ba368be8920
grasshopper7 <grass.hopper.moc@gmail.com>
2022-05-12 12:59:14 +0530

    Updated version

--- cucumber-pdf-plugin-report/src/test/java/stepdefs/RunCukeIT.java
+++ cucumber-pdf-plugin-report/src/test/java/stepdefs/RunCukeIT.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.junit.CucumberOptions;*/
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/stepdefs", plugin = { "json:target/cucumber-json.json",
+@CucumberOptions(features = "src/test/resources/stepdefs/", plugin = { "json:target/cucumber-json.json",
 		"summary" }/* , tags = "@tabledoc or @exception" */)
 public class RunCukeIT extends AbstractTestNGCucumberTests {
 
